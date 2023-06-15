ad4cabc618bde4b48c664a886105d89a0a6962b9
Iammarv0h <marvinaliero161@gmail.com>
2023-06-15 03:46:10 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints with a new line using puts function
+ * Return: 0 (this shows success in running the code)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
