7d1a6a9d40127f02bc1c642a72559102203a8048
Iammarv0h <marvinaliero161@gmail.com>
2023-06-15 04:38:22 -0700

    using sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,17 @@
  */
 int main(void)
 {
-	printf("Size of char: %zu byte(s)\n", sizeof(char));
-	printf("Size of int: %zu byte(s)\n", sizeof(int));
-	printf("size of long int: %zu byte(s)\n", sizeof(long int));
-	printf("size of long long int: %zu byte(s)\n" sizeof(long long int));
-	printf("size of float: %zu byte(s)\n" sizeof(float));
-	return (0);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+
+printf("Size of char: %lu byte(s)\n", (unsigned int)sizeof(a));
+printf("Size of int: %lu byte(s)\n", (unsigned int)sizeof(b));
+printf("size of long int: %lu byte(s)\n", (unsigned int)sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned int)sizeof(d));
+printf("size of float: %lu byte(s)\n", (unsigned int)sizeof(f));
+return (0);
 }
