61c1d3482abf1b71f97eff7b39aa2f5a69b58935
Iammarv0h <marvinaliero161@gmail.com>
2023-06-16 10:00:00 -0700

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the program starts here
+ * Return: 0(compiled successfully)
+ */
+
+int main(void)
+{
+	for (char c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
