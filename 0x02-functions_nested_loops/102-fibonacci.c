21787c915aa627de46012e67fff4f53b2cf5e4ed
Iammarv0h <marvinaliero161@gmail.com>
2023-06-20 08:45:12 -0700

    50 fib numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,62 +1,22 @@
 #include <stdio.h>
-
 /**
- * main - Prints the first 50 Fibonacci numbers
- *
- * Return: Always 0
+ * main - a function that prints 50 fib numbers
+ * Return: 0(compiled sucessfully)
  */
 int main(void)
 {
-    int term1 = 1, term2 = 2, temp;
-    int count;
+	int count;
 
-    // Print the first two terms
-    putchar('1');
-    putchar(',');
-    putchar(' ');
-    putchar('2');
+	unsigned long long a = 1, b = 2, next;
 
-    for (count = 3; count <= 50; count++)
-    {
-        temp = term1 + term2;
-
-        // Print the current term
-        putchar(',');
-        putchar(' ');
-        print_number(temp);
-
-        term1 = term2;
-        term2 = temp;
-    }
-
-    // Print a newline character
-    putchar('\n');
-
-    return 0;
+	printf("%llu, %llu", a, b);
+	for (count = 3; count <= 50; count++)
+	{
+		next = a + b;
+		printf(", %llu", next);
+		a = b;
+		b = next;
+	}
+	printf("\n");
+	return (0);
 }
-
-/**
- * print_number - Prints a number to the standard output
- *
- * @num: The number to print
- */
-void print_number(int num)
-{
-    if (num == 0)
-    {
-        putchar('0');
-        return;
-    }
-
-    if (num < 0)
-    {
-        putchar('-');
-        num = -num;
-    }
-
-    if (num / 10)
-        print_number(num / 10);
-
-    putchar((num % 10) + '0');
-}
-
