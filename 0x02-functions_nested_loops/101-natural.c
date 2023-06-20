0a31ec7af9eaae4d4a7fc9688d70af55a41b64ca
Iammarv0h <marvinaliero161@gmail.com>
2023-06-20 10:37:45 -0700

    244293

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 /**
  * main - natural numbers below 10
- * Return: 0(cmpiled successfully)
+ * Return: 0(cmpiled successfully 
  */
 
 int main(void)
 {
-	int limit = 1024;
+	int i, sum = 0;
 
-	int sum = 0;
-
-	for (int i = 3; i < limit; i++)
+	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
-		{
+		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
-		}
 	}
+
 	printf("%d\n", sum);
+
 	return (0);
 }
