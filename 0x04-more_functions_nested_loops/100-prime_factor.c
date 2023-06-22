b2409f16426d82f2552da8abfb9e33248ad69d80
Iammarv0h <marvinaliero161@gmail.com>
2023-06-22 11:27:33 -0700

    recursive

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,32 +1,24 @@
 #include <stdio.h>
-
+#include <math.h>
 /**
- * largest_prime_factor - Finds the largest prime factor of a number
- * @n: The number
+ * main - prints the larest prime factor
  *
- * Return: The largest prime factor
+ * Return: 0(compiled succesfully)
  */
-long largest_prime_factor(long n)
-{
-	long factor = 2;
-	while (factor <= n)
-	{
-		if (n % factor == 0)
-		{
-			n /= factor;
-		}
-		else
-		{
-			factor++;
-		}
-	}
-	return factor;
-}
 
 int main(void)
 {
+	long x, maxf;
 	long number = 612852475143;
-	long largest_factor = largest_prime_factor(number);
-	printf("%ld\n", largest_factor);
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
 	return (0);
 }
