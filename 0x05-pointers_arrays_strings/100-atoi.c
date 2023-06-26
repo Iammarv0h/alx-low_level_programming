f73f5bcf3ce60165f3dedfee8445661d79236365
Iammarv0h <marvinaliero161@gmail.com>
2023-06-26 07:53:04 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,17 +12,31 @@ int _atoi(char *s)
 	int result = 0;
 	int i = 0;
 
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
 	while (s[i] != '\0')
 	{
-		if (s[i] == '-')
-			sign *= -1;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
-				break;
-		}
-		i++;
+	if (s[i] >= '0' && s[i] <= '9')
+	{
+	int digit = s[i] - '0';
+
+	if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
+	{
+		if (sign == 1)
+			return (INT_MAX);
+		else
+			return (INT_MIN);
+	}
+		result = result * 10 + digit;
+	}
+	else
+	{
+		break;
+	}
+	i++;
 	}
 	return (sign * result);
 }
