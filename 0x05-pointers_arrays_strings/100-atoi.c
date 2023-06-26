d1babc2b2c307172865f814032bb6fa8e04fdd73
Iammarv0h <marvinaliero161@gmail.com>
2023-06-26 07:40:51 -0700

    I_am

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,7 @@
 
 /**
  * _atoi - Converts a string to an integer
- * @s: The string to be converted
+ * @s: The string to convert
  *
  * Return: The converted integer
  */
@@ -12,20 +12,15 @@ int _atoi(char *s)
 	int result = 0;
 	int i = 0;
 
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
 	while (s[i] != '\0')
 	{
+		if (s[i] == '-')
+			sign *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - '0');
-		}
-		else if (result != 0)
-		{
-			break;
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+				break;
 		}
 		i++;
 	}
