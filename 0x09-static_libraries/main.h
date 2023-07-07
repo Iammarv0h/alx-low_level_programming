d27f3db243e11a0809636eb49efd10d239aef017
Iammarv0h <marvinaliero161@gmail.com>
2023-07-07 14:16:10 +0300

    1. Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -11,7 +9,7 @@ void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest,const char *src, int n);
+char *_strncat(char *dest, const char *src, int n);
 char *_strncpy(char *dest, const char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
@@ -20,4 +18,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
