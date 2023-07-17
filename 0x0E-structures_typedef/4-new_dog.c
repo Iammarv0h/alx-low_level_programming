4c37108eb9cc33f6345de0b448e077a986fb0ad8
Iammarv0h <matucharleswachira@gmail.com>
2023-07-18 01:05:37 +0300

    dog.h

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+typedef struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
+
+/* Function prototype */
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif /* DOG_H */
