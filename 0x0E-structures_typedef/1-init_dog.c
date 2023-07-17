bada8db30d602e34a1f288c4eb5ac875d95b603e
Iammarv0h <matucharleswachira@gmail.com>
2023-07-18 01:54:28 +0300

    2-prin_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,21 @@
-#ifndef DOG_H
-#define DOG_H
+#include <stdlib.h>
+#include "dog.h"
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-#endif /* DOG_H */
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @d: typedef
+ * @name: dog's name
+ * @age: age of dog
+ * @owner: owner name
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,44 @@
-#ifndef DOG_H
-#define DOG_H
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
 
 /**
- * struct dog - Structure representing a dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * new_dog - newdoog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owaner name
+ *
+ * Return: pnt ndog
  */
-typedef struct dog
+dog_t *new_dog(char *name, float age, char *owner)
 {
-    char *name;
-    float age;
-    char *owner;
-} dog_t;
+	dog_t *ndog;
+	int n1, n2;
 
-/* Function prototype */
-dog_t *new_dog(char *name, float age, char *owner);
+	n1 = strlen(name);
+	n2 = strlen(owner);
 
-#endif /* DOG_H */
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+
+	ndog->name = malloc(sizeof(char) * (n1 + 1));
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	ndog->owner = malloc(sizeof(char) * (n2 + 1));
+	if (ndog->owner == NULL)
+	{
+		free(ndog);
+		free(ndog->name);
+		return (NULL);
+	}
+	strcpy(ndog->name, name);
+	strcpy(ndog->owner, owner);
+	ndog->age = age;
+
+	return (ndog);
+}
