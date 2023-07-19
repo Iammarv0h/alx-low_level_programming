4af0dae3065d2bbbc7246c61d48e1341c72c8524
Iammarv0h <marvinaliero161@gmail.com>
2023-07-19 21:03:00 +0300

    error handling

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,16 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
- * print_name - function that prints a name.
- * @name: string
- * @f: function
+ * print_name - prints a name
+ *
+ * @name: string input
+ * @f: callback function
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
 	f(name);
 }
