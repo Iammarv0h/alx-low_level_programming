a8250013fde8afcd0211870bb01e4cded0a58478
Iammarv0h <marvinaliero161@gmail.com>
2023-07-19 20:55:53 +0300

    error handling

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,39 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
- * handle_error - Handle errors and exit the program with the given status
- * @status: The exit status
- * @message: The error message to print
- */
-void handle_error(int status, const char *message)
-{
-	printf("%s\n", message);
-	exit(status);
-}
-
-/**
- * main - Print results of the operation
- * @argc: The number of arguments
- * @argv: Array of pointers to args
- * Return: Always 0
+ * main - print results of the operation
+ * @argc: the number of arguments
+ * @argv: array of pointers to args
+ * Return: ALways 0
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int n1, n2, result;
+	int n1, n2;
 	char *op;
 
 	if (argc != 4)
-		handle_error(98, "Error");
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
 	op = argv[2];
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
-		handle_error(99, "Error");
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	else if ((*op == '%' && n2 == 0) || (*op == '/' && n2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-	if ((*op == '%' || *op == '/') && n2 == 0)
-		handle_error(100, "Error");
-
-	result = get_op_func(op)(n1, n2);
-	printf("%d\n", result);
+	printf("%d\n", get_op_func(op)(n1, n2));
 
 	return (0);
 }
