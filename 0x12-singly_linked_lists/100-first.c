c45caca19a83ceff73ef0337ba6d15cf90454da3
Iammarv0h <marvinaliero161@gmail.com>
2023-07-27 18:40:58 +0300

    the hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myStartupFun - Constructor function to be executed before main.
+ */
+void myStartupFun(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
