fa1dbd5c2d7a5473bf16b34ce15fd433075a6646
Iammarv0h <marvinaliero161@gmail.com>
2023-08-07 19:30:16 +0300

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,6 +4,14 @@
 #include <fcntl.h>
 #include "main.h"
 
+/**
+ * read_textfile - Read and print a specified number of letters from a file.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure.
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
